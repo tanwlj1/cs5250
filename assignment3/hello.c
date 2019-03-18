b686f574e164a2aaa72f385c70ad7c30437fb926
tanwlj1 <tanwlj1@hotmail.com>
2019-03-19 00:28:10 +0800

    Update hello.c

--- assignment3/hello.c
+++ assignment3/hello.c
@@ -1,15 +1,23 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+
 MODULE_LICENSE("GPL");
+static char *who = "tester";
+module_param(who,charp,0000);
+MODULE_PARM_DESC(who, "parameter who");
+
 static int hello_init(void)
 {
-printk(KERN_ALERT "Hello, world\n");
-return 0;
+	printk(KERN_ALERT "Hello, %s\n", who); 
+	return 0;
 }
+
 static void hello_exit(void)
 {
-printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
+
 module_init(hello_init);
 module_exit(hello_exit);
